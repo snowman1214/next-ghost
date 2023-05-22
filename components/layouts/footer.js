2efbe50a7700dfef7f8b54a5876b698902dc36b6
web3group <jq@usedemand.com>
2023-05-23 00:19:23 +0200

    Update footer.js

--- components/layouts/footer.js
+++ components/layouts/footer.js
@@ -8,9 +8,7 @@ const Footer = () => {
       <div className="wrapper xs:grid xs:grid-2 s:grid-12">
         <div className="logo-mark xs:col-start-1 xs:col-span-1">
           <a href="index" aria-label="AI logo mark">
-            <div className="logo">
-              <Image src={logo} alt="dd"></Image>
-            </div>
+
             <svg
               width="46"
               height="32"
@@ -18,10 +16,9 @@ const Footer = () => {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                d="M32.73 0h-6.945L38.45 32h6.945L32.73 0ZM12.665 0 0 32h7.082l2.59-6.72h13.25l2.59 6.72h7.082L19.929 0h-7.264Zm-.702 19.337 4.334-11.246 4.334 11.246h-8.668Z"
-                fill="#FAFAF7"
-              />
+              <div className="logo">
+                <Image src={logo} alt="dd"></Image>
+              </div>
             </svg>
           </a>
         </div>
@@ -98,7 +95,7 @@ const Footer = () => {
           </ul>
         </div>
         <div className="footer-bottom xs:col-start-1 xs:col-span-2 s:col-start-5 s:col-span-8 m:col-start-11 m:col-span-2">
-          <div className="copyright">&copy; 2023 Anthropic PBC</div>
+          <div className="copyright">&copy; 2023 Suisseblocks Group</div>
         </div>
       </div>
     </footer>
